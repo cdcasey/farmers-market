c15982eadb74d9ab42420c8821c1825f58264301
Chris Casey <cdcasey@gmail.com>
2018-12-11 22:25:09 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -5,49 +5,50 @@ import ItemTable from './components/ItemTable';
 import Cart from './components/Cart';
 
 import { inventory } from './market';
+// import { addItemToCart } from './redux/actions';
 
 class App extends Component {
-    state = {
-        itemsInCart: []
-    };
+  state = {
+    itemsInCart: []
+  };
 
-    addItemToCart(event) {
-        const { id } = event.target;
-        this.setState((state) => ({
-            itemsInCart: [...state.itemsInCart, inventory[id]]
-        }));
-    }
+  addItemToCart(event) {
+    const { id } = event.target;
+    this.setState((state) => ({
+      itemsInCart: [...state.itemsInCart, inventory[id]]
+    }));
+  }
 
-    removeItemFromCart(event) {
-        const { cartIndex } = event.target.dataset;
+  removeItemFromCart(event) {
+    const { cartIndex } = event.target.dataset;
 
-        const itemsInCart = this.state.itemsInCart.filter(
-            (item, i) => i !== Number(cartIndex)
-        );
+    const itemsInCart = this.state.itemsInCart.filter(
+      (item, i) => i !== Number(cartIndex)
+    );
 
-        this.setState((state) => ({
-            itemsInCart
-        }));
-    }
+    this.setState((state) => ({
+      itemsInCart
+    }));
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container">
-                    <Header cart={this.state.itemsInCart} />
-                    <div className="container__tables">
-                        <ItemTable add={this.addItemToCart.bind(this)} />
-                        <div>
-                            <Cart
-                                cart={this.state.itemsInCart}
-                                removeItem={this.removeItemFromCart.bind(this)}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <Header cart={this.state.itemsInCart} />
+          <div className="container__tables">
+            <ItemTable add={this.addItemToCart.bind(this)} />
+            <div>
+              <Cart
+                cart={this.state.itemsInCart}
+                removeItem={this.removeItemFromCart.bind(this)}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
